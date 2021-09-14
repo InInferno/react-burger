a8e31aa79914e199736a513d4ecb68af64d35b68
Vadim Ivanov <vadim1ivanov@yandex.ru>
2021-09-14 23:49:24 +0300

    Added request to API

--- src/components/burger-container/burger-container.js
+++ src/components/burger-container/burger-container.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './burger-container.module.css';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
-import data from '../../utils/data';
+import { url } from '../../utils/config'
 
 export default function BurgerContainer() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then(res => {
+        setData(res.data);
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
+
   return (
     <main className={styles.box}>
         <h1 className="text text_type_main-large mt-10 mb-5">
