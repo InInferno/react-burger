0324f9aaabd4675ad33e426c8a830afae31c0418
Vadim Ivanov <vadim1ivanov@yandex.ru>
2021-10-24 01:45:40 +0300

    Add Orders

--- src/pages/orders/orders.js
+++ src/pages/orders/orders.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styles from './orders.module.css';
 import OrderList from '../../components/order-list/order-list';
+import NavProfile from '../../components/nav-profile/nav-profile';
 
 
 export default function Orders() {
 
   return (
-    <div>
-      <OrderList />
+    <div className={styles.container}>
+      <div className='mt-20'>
+        <NavProfile />
+        <p className={`${styles.description} text text_type_main-default text_color_inactive mt-20`}>
+          В этом разделе вы можете изменить свои персональные данные
+        </p>
+      </div>
+      <div className='mt-10'>
+        <OrderList />
+      </div>
     </div>
   );
 }
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './profile.module.css';
 import { Input, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import { logoutFetch, updateUserInfoFetch } from '../../services/actions/profile-actions';
+import { updateUserInfoFetch } from '../../services/actions/profile-actions';
 import { useDispatch, useSelector } from 'react-redux';
 import NavProfile from '../../components/nav-profile/nav-profile';
 
@@ -47,80 +47,76 @@ function Profile() {
     
   return (
     <div className={styles.container}>
-        <div className={styles.box}>
-            <div className='mr-15 mt-20'>
-                <NavProfile />
-            </div>
-            <form 
-                className={`${styles.form} mt-20`}
-                onSubmit={saveHandled}
-            >
-                <Input
-                    disabled
-                    type={'text'}
-                    placeholder={'Имя'}
-                    onChange={e => setName(e.target.value)}
-                    icon={'EditIcon'}
-                    value={name}
-                    name={'name'}
-                    error={false}
-                    ref={inputNameRef}
-                    onIconClick={onIconClickName}
-                    errorText={'Ошибка'}
-                    size={'default'}
-                />
-                <Input
-                    disabled={true}
-                    type={'email'}
-                    placeholder={'Логин'}
-                    onChange={e => setEmail(e.target.value)}
-                    icon={'EditIcon'}
-                    value={email}
-                    name={'email'}
-                    error={false}
-                    ref={inputLoginRef}
-                    onIconClick={onIconClickLogin}
-                    errorText={'Ошибка'}
-                    size={'default'}
-                />
-                <Input
-                    disabled={true}
-                    type={'password'}
-                    placeholder={'Пароль'}
-                    onChange={e => setPassword(e.target.value)}
-                    icon={'EditIcon'}
-                    value={password}
-                    name={'password'}
-                    error={false}
-                    ref={inputPasswordRef}
-                    onIconClick={onIconClickPassword}
-                    errorText={'Ошибка'}
-                    size={'default'}
-                />
-
-                <div className={styles.buttons}>
-                    <div className={styles.button}>
-                        <Button type="primary" size="medium">
-                            Сохранить
-                        </Button>
-                    </div>
-                    <div className={styles.button}>
-                        <Button 
-                            type="primary" 
-                            size="medium" 
-                            onClick={(() => onClickCancel())}
-                        >
-                            Отмена
-                        </Button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div className={`${styles.info} mt-20`}>
-            <p className={`${styles.description} text text_type_main-default text_color_inactive`}>
+        <div className='mt-20'>
+            <NavProfile />
+            <p className={`${styles.description} text text_type_main-default text_color_inactive mt-20`}>
                 В этом разделе вы можете изменить свои персональные данные
             </p>
         </div>
+        <form
+            className={`${styles.form} mt-20`}
+            onSubmit={saveHandled}
+        >
+            <Input
+                disabled
+                type={'text'}
+                placeholder={'Имя'}
+                onChange={e => setName(e.target.value)}
+                icon={'EditIcon'}
+                value={name}
+                name={'name'}
+                error={false}
+                ref={inputNameRef}
+                onIconClick={onIconClickName}
+                errorText={'Ошибка'}
+                size={'default'}
+            />
+            <Input
+                disabled={true}
+                type={'email'}
+                placeholder={'Логин'}
+                onChange={e => setEmail(e.target.value)}
+                icon={'EditIcon'}
+                value={email}
+                name={'email'}
+                error={false}
+                ref={inputLoginRef}
+                onIconClick={onIconClickLogin}
+                errorText={'Ошибка'}
+                size={'default'}
+            />
+            <Input
+                disabled={true}
+                type={'password'}
+                placeholder={'Пароль'}
+                onChange={e => setPassword(e.target.value)}
+                icon={'EditIcon'}
+                value={password}
+                name={'password'}
+                error={false}
+                ref={inputPasswordRef}
+                onIconClick={onIconClickPassword}
+                errorText={'Ошибка'}
+                size={'default'}
+            />
+
+            <div className={styles.buttons}>
+                <div className={styles.button}>
+                    <Button type="primary" size="medium">
+                        Сохранить
+                    </Button>
+                </div>
+                <div className={styles.button}>
+                    <Button 
+                        type="primary" 
+                        size="medium" 
+                        onClick={(() => onClickCancel())}
+                    >
+                        Отмена
+                    </Button>
+                </div>
+            </div>
+        </form>
     </div>
   );
 }
