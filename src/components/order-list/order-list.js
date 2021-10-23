3ec65796203045025bf9da36bd515662f3fe2130
Vadim Ivanov <vadim1ivanov@yandex.ru>
2021-10-23 23:49:52 +0300

    Update OrderList

--- src/components/order-list/order-list.js
+++ src/components/order-list/order-list.js
@@ -1,209 +1,49 @@
 import React from 'react';
 import styles from './order-list.module.css';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-
+import { dataOrders } from '../../utils/constants'
 
 export default function OrderList() {
 
+  const data = dataOrders;
+  
   return (
     <div className={`${styles.container} ${styles.scroll}`}>
       <div className={`${styles.cards} mr-2`}>
-        <div className={`${styles.card}`}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
+
+        {data.map((item, index) => {
+          
+          return <div
+            className={styles.card}
+            key={index}
+          >
+            <div className={styles.info}>
+              <p className='text text_type_digits-default'>{item.id}</p>
+              <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
+            </div>
+            <p className='text text_type_main-medium mt-6'>{item.name}</p>
+            <div className={`${styles.order} mt-6`}>
+              <div className={styles.images}>
+                {item.images.map((image, index) => {
+                  if (index < 6) { 
+                    return (
+                      <div key={index} style={{zIndex: item.images.length - index}} className={`${styles.radius} ${item.images.length > 6 && index === 5 && styles.blackout}`}>
+                        <img className={`${styles.image}`} src={image} alt='Ингредиент' />
+                        {item.images.length > 6 && index === 5 && 
+                          <p className={`${styles.number} text text_type_digits-default`}>{`+${item.images.length - 6}`}</p>
+                        }
+                      </div>
+                    )
+                  }
+                })}
               </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
+              <div className={styles.price}>
+                <p className="text text_type_digits-medium mr-2">{item.price}</p>
+                <CurrencyIcon type="primary"/>
               </div>
             </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
           </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={`${styles.radius} ${styles.dark}`}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-                <p className={`${styles.number} text text_type_digits-default`}>+3</p>
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <p className='text text_type_digits-default'>#034535</p>
-            <p className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</p>
-          </div>
-          <p className='text text_type_main-medium mt-6'>Death Start Starship Main бургер</p>
-          <div className={`${styles.order} mt-6`}>
-            <div className={styles.images}>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-02.png'} alt='Ингредиент' />
-              </div>
-              <div className={styles.radius}>
-                <img className={styles.image} src={'https://code.s3.yandex.net/react/code/bun-01.png'} alt='Ингредиент' />
-              </div>
-            </div>
-            <div className={styles.price}>
-              <p className="text text_type_digits-medium mr-2">666</p>
-              <CurrencyIcon type="primary"/>
-            </div>
-          </div>
-        </div>
+        })}
       </div>
     </div>
   )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,53 @@
 export const url = 'https://norma.nomoreparties.space/api';
+
+export const dataOrders = [
+    {
+      id: '#111111',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Death Start Starship Main бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-02.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-02.png'],
+      price: '666',
+    },
+    {
+      id: '#222222',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Interstellar бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-01.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-01.png'],
+      price: '1337',
+    },
+    {
+      id: '#333333',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Black Hole Singularity острый бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-02.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/bun-02.png'],
+      price: '228',
+    },
+    {
+      id: '#444444',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Supernova Infinity бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-01.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-01.png'],
+      price: '42',
+    },
+    {
+      id: '#555555',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Death Start Starship Main бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-01.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-01.png'],
+      price: '13',
+    },
+    {
+      id: '#666666',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Death Start Starship Main бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-01.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-01.png'],
+      price: '777',
+    },
+    {
+      id: '#777777',
+      date: 'Сегодня, 16:20 i-GMT+3',
+      name: 'Death Start Starship Main бургер',
+      images: ['https://code.s3.yandex.net/react/code/bun-01.png', 'https://code.s3.yandex.net/react/code/sauce-02.png', 'https://code.s3.yandex.net/react/code/sauce-01.png', 'https://code.s3.yandex.net/react/code/meat-03.png', 'https://code.s3.yandex.net/react/code/meat-01.png', 'https://code.s3.yandex.net/react/code/mineral_rings.png', 'https://code.s3.yandex.net/react/code/cheese.png', 'https://code.s3.yandex.net/react/code/bun-01.png'],
+      price: '666',
+    }
+]
